5e96f1778243164fad81409859a4af6188123627
Laxmanmallick1997 <93567151+Laxmanmallick1997@users.noreply.github.com>
2022-03-28 17:37:48 +0530

    initial commit

---
+++ src/Components/NotFound/NotFound.jsx
@@ -0,0 +1,6 @@
+export const NotFound = () => {
+  return <div className="notFound">
+    {/* Show some 404 not found image or component here */}
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeS0rCv6z3N6oUG4gLlhM6-9GfhA1I_eMEZDwMUqvncWLp8PhQvkNiwbTUUA7zahr0JHY&usqp=CAU" alt="404"></img>
+  </div>
+};
